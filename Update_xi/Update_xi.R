b95973ff77f28400d8424657358ac9b03e777360
davidecaffu <davide.caffu@mail.polimi.it>
2021-12-02 16:07:38 +0100

    update xi

--- Update_xi/Update_xi.R
+++ Update_xi/Update_xi.R
@@ -1,16 +1,22 @@
 ##### UPDATE XIs
 
 
-update_xi <- function(Y, S, k, xi_old_mean, xi_old_sd, mu0, k0, nu0 ){
-  for (j in [1:k]){
+update_xi <- function(Y, S, k, xi_old_mean, xi_old_sd, mu0, k0, lambda0, nu0){
+  for (j in 1:k){
     Yj = Y[S==j]    # select data belonging to cluster j
-    S0 = diag(cov(Yj))
-    n = len(Yj)
-    mu_n = (k0*mu0+n*mean(Yj))/(k0+n)
+    sample_cov = cov(Yj)
+    n = dim(Yj)[1]
+    d = dim(Yj)[2]
+    # Compute the parameters for the marginals
     k_n = k0 + n
+    mu_n = (k0*mu0+n*mean(Yj))/k_n
     nu_n = nu0 + n
-    Sn = S0
+    lambda_n = lambda_0 + sample_cov + k0*n/k_n*(mean(Yj)-mu0)%*%t((mean(Yj)-mu0))
     
+    xi_sd = rinvwishart(nu_n, inv(lambda_n))
+    
+    df = nu_n-d+1
+    xi_mu = rmvt(1, mu_n, lambda_n/k_n/df, df)
     
   }
   

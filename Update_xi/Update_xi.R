868275e8639cb43ddb770d4aaefe592d3180e991
davidecaffu <davide.caffu@mail.polimi.it>
2021-12-02 17:45:32 +0100

    ...

--- Update_xi/Update_xi.R
+++ Update_xi/Update_xi.R
@@ -20,9 +20,19 @@ update_xi <- function(Y, S, k, mu0, k0, lambda0, nu0){
     xi_mu_j = rmvt(1, mu_n, lambda_n/k_n/df, df)
     xi_sigma_j = rinvwishart(nu_n, inv(lambda_n))
     
-    xi_mu <- list(xi_mu, list(xi_mu_j))
-    xi_sigma <- list(xi_sigma, list(xi_sigma_j))
+    xi_mu <- append(xi_mu, list(xi_mu_j))
+    xi_sigma <- append(xi_sigma, list(xi_sigma_j))
   }
   
   return(list("xi_mean" = xi_mu, "xi_sigma" = xi_sigma))
 }
+
+# How the created data structure works:
+# list of lists where each inner list contains a vector and a matrix
+# sigma = diag(2)
+# l = list()
+# l = append(l, list(sigma))
+# l
+
+
+

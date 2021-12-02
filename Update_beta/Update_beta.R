232f2b660cf35f174e578741f4f678a258ccf738
GretaGalliani <greta.galliani@gmail.com>
2021-12-02 17:40:57 +0100

    Some comments added to update_beta

--- Update_beta/Update_beta.R
+++ Update_beta/Update_beta.R
@@ -9,7 +9,6 @@
 
 # OUTPUT: beta -> value of the parameter beta at the r iteration
 #         acc -> number of accepted proposals at the current iteration
-
 update_beta <- function(n, m1_bar, beta_old, sd = 2, n_acc) { 
   # Extraction of a new value from the proposal distribution, doing an appropriate transformation 
   # to correct the fact that beta is in (0,1)
@@ -40,7 +39,6 @@ update_beta <- function(n, m1_bar, beta_old, sd = 2, n_acc) {
 # Function to change the value of beta so beta_star is in (-inf, +inf) to do MH
 # INPUT: x -> value of beta, x is in (0,1)
 # OUTPUT: x_star -> value of transformed beta, x_star is in (-inf, +inf)
-
 change_beta <- function(x) {
   return (log(x) - log(1-x))
 }
@@ -49,7 +47,6 @@ change_beta <- function(x) {
 # inv_beta is the inverse function of change_beta
 # INPUT: x -> value of beta_star, x is in (-inf, +inf)
 # OUTPUT: x_star -> value of beta, x_star is in (0, 1)
-
 inv_beta <- function(x) {
   return (exp(x) / (exp(x) + 1))
 }
@@ -60,7 +57,6 @@ inv_beta <- function(x) {
 #        n -> number of data points
 #        m1_bar -> number of points in group 0
 # OUTPUT: alpha -> the alpha needed to perform the acceptance/rejection in MH
-
 calcolo_alpha_beta <- function(x, y, n, m1_bar) {
   # Computation of the partial posterior density for y and x
   pi_y <- dens_beta(y, n, m1_bar)
@@ -79,7 +75,6 @@ calcolo_alpha_beta <- function(x, y, n, m1_bar) {
 #        n -> number of data points 
 #        m1_bar -> number of points in group 0
 # OUTPUT: alpha -> the alpha needed to perform the acceptance/rejection in MH
-
 dens_beta <- function(x, n, m1_bar) {
   return ( x^(n - m1_bar) * (1 - x)^(m1_bar) * 1/(x*(1-x)))
 }

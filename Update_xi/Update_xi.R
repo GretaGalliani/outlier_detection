0ac124b9e82927146abaadfb53f138aed2b28ce3
GretaGalliani <greta.galliani@gmail.com>
2021-12-09 14:44:38 +0100

    update_xi commented

--- Update_xi/Update_xi.R
+++ Update_xi/Update_xi.R
@@ -1,35 +1,54 @@
 ##### UPDATE XIs
 library(LaplacesDemon)
 
+# Function to update the groups' parameters (mu and cov)
+# INPUT: Y -> data 
+#        S -> vector of labels Si i=1,...,n
+#        k -> number of distinct groups
+#        Q_param -> parameters of the prior for Q
+
+# OUTPUT: xi_mu_star -> list xi_mu_starj j=1,...,k which contains for each group the  value of mu 
+#         xi_cov_star -> list xi_cov_starj j=1,...,k which contains for each group the value of cov
 update_xi <- function(Y, S, k, Q_param){
   
-  mu_0 = Q_param$mu_0
-  k_0 = Q_param$k_0
-  lambda_0 = Q_param$lambda_0
-  nu_0 = Q_param$nu_0
-  
+  # Creation of the lists for mu and cov
   xi_mu = list()
   xi_sigma = list()
   
+  # For each group
   for (j in 1:k){
-    Yj = Y[S==j]    # select data belonging to cluster j
-    sample_cov = cov(Yj)
-    n = dim(Yj)[1]
-    d = dim(Yj)[2]
-    # Compute the parameters for the marginals
-    k_n = k0 + n
-    mu_n = (k0*mu0+n*mean(Yj))/k_n
-    nu_n = nu0 + n
-    lambda_n = lambda_0 + sample_cov + k0*n/k_n*(mean(Yj)-mu0)%*%t((mean(Yj)-mu0))
+    # Select data belonging to cluster j
+    Yj <- Y[which(S==j),]
     
+    # Sample covariance
+    sample_cov = cov(Yj)
+    
+    # Extraction of the number of data points
+    n = dim(Yj)[1]
+    
+    # Extraction of the dimension of data
+    d = dim(Yj)[2]
+    
+    # Compute the parameters for the marginals
+    k_n = Q_param$k_0 + n
+    mu_n = (Q_param$k_0*Q_param$mu_0+n*mean(Yj))/k_n
+    nu_n = Q_param$nu_0 + n
+    lambda_n = Q_param$lambda_0 + sample_cov + Q_param$k_0*n/k_n*(mean(Yj)-Q_param$mu_0)%*%t((mean(Yj)-Q_param$mu_0))
+    
+    # Application of inversion based on cholesky decomposition
     lambda_n_chol <- chol(lambda_n)
     inv_lambda_n_chol <- chol2inv(lambda_n_chol)
     
+    # computation of degrees of freedom
     df = nu_n-d+1
+    
+    # Sampling of mu (from a multivariate t-student)
     xi_mu_j = rmvt(1, mu_n, lambda_n/k_n/df, df)
     
+    # Sampling of cov (from an inverse-wishart), based on cholesky decomposition
     xi_sigma_j = rinvwishartc(nu_n, chol(inv_lambda_n_chol))
     
+    # Appending of the sampled values
     xi_mu <- append(xi_mu, list(xi_mu_j))
     xi_sigma <- append(xi_sigma, list(xi_sigma_j))
   }

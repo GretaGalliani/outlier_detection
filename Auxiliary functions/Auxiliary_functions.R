4ef404b72f53d58738a64270e4a461bbdc6dd303
GretaGalliani <greta.galliani@gmail.com>
2021-12-09 12:14:37 +0100

    auxiliary_functions commented

--- Auxiliary functions/Auxiliary_functions.R
+++ Auxiliary functions/Auxiliary_functions.R
@@ -1,19 +1,22 @@
 ##### AUXILIARY FUNCTIONS
 
-# Function computing m1_bar
-# i.e. computing the number of samples classified as outliers (labeled with 0)
-# Input: vector of labels Si i=1,...,n
-# ALREADY TESTED
+# Function to compute m1_bar (number of points in group 0)
+# INPUT: S -> vector of labels Si i=1,...,n
+# OUTPUT: m1_bar -> number of points in group 0
 m1_bar <- function(S){
   return (sum(S==0))
 }
 
-# Function computing m1
-# i.e. computing the number of singletons: the outliers and the groups
-# containing only one sample
-# ALREADY TESTED
+# Function to compute m1 (number of singletons (number of points in group 0 + number of groups with one point))
+# INPUT: S -> vector of labels Si i=1,...,n
+# OUTPUT: m1 -> number of singletons
 m1 <- function(S){
+  # Computation of the number of points in group 0, which are all singletons
   m1_bar = m1_bar(S)
+  
+  # Computation of the frequencies of all groups, excluding group 0
   x = S[S!=0]
+  
+  # I return the number of singletons
   return (m1_bar + sum(table(x)==1))
 }

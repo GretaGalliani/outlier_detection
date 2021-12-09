8a903f75332ce7c46c7c15c90803c28963a1d9c0
GretaGalliani <greta.galliani@gmail.com>
2021-12-09 15:10:04 +0100

    First_try

--- First_try.R
+++ First_try.R
@@ -1,4 +1,5 @@
 library(MASS)
+source("main.R")
 
 mu_1 <- c(0,0)
 Sigma_1 <- matrix(c(1,0,0,1), nrow = 2, ncol = 2)
@@ -12,3 +13,6 @@ data_2 <- mvrnorm(n = 5, mu_1, Sigma_1)
 
 data = rbind(data_1, data_2)
 
+
+
+algorithm <- function(Y, S_init, sigma_init, theta_init, beta_init, xi_mu, xi_cov, Q_param, P_param, n_iter )

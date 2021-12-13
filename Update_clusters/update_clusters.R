6b9f7c11b867a2a4fc524a5d6b800709a6c8600a
GretaGalliani <greta.galliani@gmail.com>
2021-12-13 12:04:31 +0100

    update_clusters looks fixed :)))))))))

--- Update_clusters/update_clusters.R
+++ Update_clusters/update_clusters.R
@@ -31,14 +31,14 @@ update_clusters <- function(Y, xi_mu_star, xi_cov_star, S_old,
                               beta_old, theta_old, sigma_old, k_old,
                               P_param, Q_param)
 {
-  N <- dim(Y)[1]
+  n <- dim(Y)[1]
   
   # Creation of the new S and k initialized like the ones at previous iteration
   curr <- S_old 
   k_new <- k_old
   
   # Cycling over all the elements
-  for (i in 1:N)
+  for (i in 1:n)
   {
     # Computation of the probability that the point 
     # - comes from the contaminated distribution --> j=0
@@ -55,7 +55,10 @@ update_clusters <- function(Y, xi_mu_star, xi_cov_star, S_old,
     p = dim(Y)[2]
     
     # Probability that the i-th element is in the contaminated part
-    prob[1] <- dens_contaminated(Y[i,], p,beta_old, P_param) 
+    prob[1] <- dens_contaminated(Y[i,], beta_old, P_param) 
+    
+    # print("prob contaminated")
+    # print(prob[1])
     
     
     #j=1:K
@@ -65,17 +68,14 @@ update_clusters <- function(Y, xi_mu_star, xi_cov_star, S_old,
     {
       # If the current group is empty the probability of sampling that group is 0
       if (!((t-1) %in% curr)){
-        prob[t-1]=0
+        prob[t]=0
       }
       
       else
       {
         # Frequency of the current group
         n_j <- sum(curr == t-1)
-        
-
-        
-
+      
         # print("n_j con i")
         # print(n_j)
         #print("gruppo")
@@ -91,7 +91,7 @@ update_clusters <- function(Y, xi_mu_star, xi_cov_star, S_old,
         # If data point i is the only point in the current group, then the probability 
         # of sampling that group is 0
         if (n_j == 1 & curr[i] == t-1){
-          prob[t-1]=0
+          prob[t]=0
         }
         
         else{
@@ -99,26 +99,39 @@ update_clusters <- function(Y, xi_mu_star, xi_cov_star, S_old,
           # Frequency of the current group without point i 
           n_j <- sum(curr[-i] == t-1)
           
-          n <- dim(data)[1]
-          
           # Computation of the probability that data i is sampled in group j
           prob[t] <- dens_cluster_old(Y[i,],n_j, n, m1_bar, sigma_old, theta_old, beta_old, xi_mu_star[[t-1]],
                                       xi_cov_star[[t-1]])
+          
+          
         }
       }
       
-      
+      # print("gruppo")
+      # print(t-1)
+      # print( "prob gruppo ")
+      # print(prob[t])
     }
     
     #j=K+1
     
     # Computation of the probability that data i is sampled in a new group 
-    prob[k_new+2]<- dens_cluster_new(Y[i,], p, N, beta_old, sigma_old, theta_old, m1_bar, k_new,
+    prob[k_new+2]<- dens_cluster_new(Y[i,], n, beta_old, sigma_old, theta_old, m1_bar, k_new,
                                      Q_param)
     
+    # print("prob gruppo nuovo")
+    # print(prob[k_new+2])
+    
+    
+    print("vector of prob")
+    print(prob)
     
     # Sampling of the new assignment
-    j <- sample(0:(k_new+1),size=1,prob=prob)
+    j <- sample(1:(k_new+2),size=1,prob=prob) - 1
+    
+    
+    print("sampling")
+    print(j)
     
     curr[i] <- j
     
@@ -139,12 +152,17 @@ update_clusters <- function(Y, xi_mu_star, xi_cov_star, S_old,
     
       
    } 
+
   
   # At the end all the empty groups are deleted and the others are shifted
   delete_list <- delete_and_shift(curr, xi_mu_star, xi_cov_star)
   curr <- delete_list$curr
   xi_mu_star <- delete_list$xi_mu_star
   xi_cov_star <- delete_list$xi_cov_star
+  
+  
+  print("curr")
+  print(curr)
   
   S_new <- curr
   my_list <-list("S_new"=S_new, "xi_mu_star"=xi_mu_star,"xi_cov_star"=xi_cov_star)
@@ -160,11 +178,25 @@ update_clusters <- function(Y, xi_mu_star, xi_cov_star, S_old,
 
 # OUTPUT: weight -> the computed probability
 
-dens_contaminated <- function(data, p, beta_old, P_param)
+dens_contaminated <- function(data, beta_old, P_param)
 {
+  data <- as.matrix(data)
+  data <- t(data)
+  p = dim(data)[2]
+  
+  mu_n = as.matrix(P_param$k_0/(P_param$k_0+1)*P_param$mu_0 + (1/P_param$k_0 + 1)*data)
+  
+  k_n = P_param$k_0 + 1
+  nu_n = P_param$nu_0 + 1
+  
+  r <- as.matrix(data-P_param$mu_0)
+  
+  lambda_n = P_param$lambda_0 + (P_param$k_0/(P_param$k_0 + 1))*t(r)%*%r
+  
   # Evaluation of a multivariate t-Student
-  weight <- (1-beta_old) *LaplacesDemon::dmvt(data,mu= P_param$mu_0, S = ((P_param$lambda_0 * (P_param$k_0+1))/(P_param$k_0*(P_param$nu_0-p+1))), 
-                                  df = P_param$nu_0-p+1)  
+  weight <- (1-beta_old) *LaplacesDemon::dmvt(data,mu= mu_n, S = lambda_n*(k_n+1)/(k_n*(nu_n-p+1)), 
+                                              df = nu_n-p+1)
+  
   return (weight)
 }
 
@@ -205,13 +237,26 @@ dens_cluster_old <- function(data, n_j, n, m1_bar, sigma_old, theta_old, beta_ol
 
 # OUTPUT: weight -> the computed probability
 
-dens_cluster_new <- function(data, p, n, beta_old, sigma_old, theta_old, m1_bar, k_old, Q_param)
+dens_cluster_new <- function(data, n, beta_old, sigma_old, theta_old, m1_bar, k_old, Q_param)
 {
-  coeff <- beta_old* (theta_old+(k_old-m1_bar)*sigma_old)/(theta_old+n-m1_bar-1)
+  data <- as.matrix(data)
+  p = dim(data)[2]
+  
+  data <- t(data)
+  
+  mu_n = as.matrix(Q_param$k_0/(Q_param$k_0+1)*Q_param$mu_0 + (1/Q_param$k_0 + 1)*data)
+  k_n = Q_param$k_0 + 1
+  nu_n = Q_param$nu_0 + 1
+  
+  r <- as.matrix(data-Q_param$mu_0)
+  
+  lambda_n = Q_param$lambda_0 + (Q_param$k_0/(Q_param$k_0 + 1))*t(r)%*%r
+  
+  coeff <- beta_old * (theta_old+(k_old-m1_bar)*sigma_old)/(theta_old+n-m1_bar-1)
   
   # Evaluation of a multivariate t-Student
-  weight <- coeff *LaplacesDemon::dmvt(data, mu = Q_param$mu_0, S = (Q_param$lambda_0 * (Q_param$k_0+1))/(Q_param$k_0*(Q_param$nu_0-p+1)), 
-                                  df = Q_param$nu_0-p+1) 
+  weight <- coeff *LaplacesDemon::dmvt(data,mu= mu_n, S = lambda_n*(k_n+1)/(k_n*(nu_n-p+1)), 
+                                                        df = nu_n-p+1)  
   return (weight)
 }
 

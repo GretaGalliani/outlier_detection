322e8fc20facb56bb4e5c19598d156189a4e8e51
NicoleFontana <nicole.fontana@mail.polimi.it>
2021-12-15 17:09:45 +0100

    ..

--- Update_sigma/Update_sigma.R
+++ Update_sigma/Update_sigma.R
@@ -116,14 +116,14 @@ dens_sigma <- function(x, k, m1, m1_bar, theta, freq, sigma_param) {
   # print(single)
   
   if(single)
-    return ( dbeta(x, sigma_param$a, sigma_param$b) * x^(k) * gamma(theta/x + k)/gamma(theta/x) * 1/(x*(1-x)))
+    return ( dbeta(x, sigma_param$a, sigma_param$b) * x^(k) * gamma(theta/x + k)/gamma(theta/x) * (1/(x*(1-x))))
   else{
     freq_m1 = freq[freq>1]
     
     # print("vettore produttoria")
     # print(gamma(freq_m1-x)/gamma(1-x))
     
-    return ( dbeta(x, sigma_param$a, sigma_param$b) * x^(k) * (gamma(theta/x + k)/gamma(theta/x)) * prod(gamma(freq_m1-x)/gamma(1-x)) * 1/(x*(1-x)))
+    return ( dbeta(x, sigma_param$a, sigma_param$b) * x^(k) * (gamma(theta/x + k)/gamma(theta/x)) * prod(gamma(freq_m1-x)/gamma(1-x)) * (1/(x*(1-x))))
   }  
 }
 

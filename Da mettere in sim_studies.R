4b4b30cafa80cc2d8347a79a7a4cfb17d3285222
GretaGalliani <greta.galliani@gmail.com>
2021-12-27 14:48:27 +0100

    Working to sim study

---
+++ Da mettere in sim_studies.R
@@ -0,0 +1,51 @@
+### LORO COSTRUISCONO I DATI 
+
+set.seed(24091998)
+
+# INIZIALIZZAZIONE - P0 DIVERSO DA Q0
+Q_param = list()
+P_param = list()
+
+d = 2
+
+Q_param$k_0 = 1
+Q_param$mu_0 = c(0,0)
+Q_param$nu_0 = d + 3 # it must be > (p-1)
+Q_param$lambda_0 = diag(diag(cov(data)))
+
+P_param$k_0 = 0.25
+P_param$mu_0 = c(0,0)
+P_param$nu_0 = d + 3 # it must be > (p-1)
+P_param$lambda_0 = diag(diag(cov(data)))
+
+n = dim(data)[1]
+S_init = rep(1,n)
+
+beta_param = list()
+sigma_param = list()
+theta_param = list()
+beta_param$a = 1
+beta_param$b = 1
+sigma_param$a = 1
+sigma_param$b = 1
+theta_param$a = 2
+theta_param$b = 0.02
+
+xi_mu <- list()
+xi_cov <- list()
+
+init_mu <- colMeans(data)
+init_var <- cov(data)
+
+for (i in 1:n){
+  xi_mu <- append(xi_mu, list(init_mu))
+  xi_cov <- append(xi_cov, list(init_var))
+}
+
+# RUNNING THE ALGORITHM 
+source("main.R")
+result <- algorithm(data, S_init, sigma_init, theta_init, beta_init, beta_param, sigma_param, theta_param, xi_mu, xi_cov, Q_param, P_param, 15000, 5000, 10)
+
+
+
+# CLUSTER ANALYSIS
